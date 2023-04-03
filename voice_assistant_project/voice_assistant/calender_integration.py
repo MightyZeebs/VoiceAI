177e31a05c5c1d9d6eb888cee7f6b194a594e41e
Zeebs <simplyninjaz@gmail.com>
2023-04-03 15:10:52 -0400

    switch from dateparser to parsedatetime

---
+++ voice_assistant_project/voice_assistant/calender_integration.py
@@ -0,0 +1,82 @@
+import datetime
+import dateparser
+import spacy
+import parsedatetime as pdt
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+nlp = spacy.load("en_core_web_sm")
+
+def get_date_info(question):
+    today = datetime.date.today()
+
+    doc = nlp(question)
+    date_detected = False
+    for token in doc:
+        if token.ent_type_ == "DATE":
+           cal = pdt.Calendar()
+           time_struct, parse_status = cal.parse(token.text, sourceTime=today.timetuple())
+           target_date = datetime.date(time_struct.tm_year, time_struct.tm_mon, time_struct.tm_mday)
+           date_detected = True
+           break
+
+    if not date_detected:
+        return "Sorry, I don't understand the question."
+    if parse_status != 0:
+        return "Sorry, I couldn't understand the date expression."
+
+    if target_date == today:
+        return f"Today is {today.strftime('%A, %B %d')}."
+    elif target_date == today + datetime.timedelta(days=1):
+        return f"Tomorrow is {target_date.strftime('%A, %B %d')}."
+    elif target_date > today and target_date < today + datetime.timedelta(days=7):
+        return f"{target_date.strftime('%A')} is {target_date.strftime('%B %d')}."
+    elif target_date.month == today.month and target_date.year == today.year:
+        days_left = (target_date - today).days
+        return f"There are {days_left} days left until {target_date.strftime('%A, %B %d')}."
+    elif target_date.year == today.year:
+        return f"{target_date.strftime('%A, %B %d')} is in {target_date.month - today.month} months."
+    else:
+        return f"{target_date.strftime('%A, %B %d, %Y')} is in {target_date.year - today.year} years."
+    
+def get_calendar_service():
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    SERVICE_ACCOUNT_FILE = 'C:\\Users\\Zeebra\\code\\Key\\speech-381402-70b45c1e2474.json'
+
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, SCOPES)
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+        return service
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+        return None
+
+def create_reminder(title, start_time, calendar_id='primary'):
+    service = get_calendar_service()
+    if service is None:
+        return "Failed to set reminder. Please check your credentials."
+
+    event = {
+        'summary': title,
+        'start': {
+            'dateTime': start_time.isoformat(),
+            'timeZone': 'UTC',  # Change this to the appropriate timezone
+        },
+        'end': {
+            'dateTime': (start_time + datetime.timedelta(minutes=15)).isoformat(),
+            'timeZone': 'UTC',  # Change this to the appropriate timezone
+        },
+        'reminders': {
+            'useDefault': True
+        },
+    }
+
+    try:
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
+        return f'Reminder created: {event["summary"]} on {event["start"]["dateTime"]}'
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+        return "Failed to set reminder."
--- voice_assistant_project/voice_assistant/openai_integration.py
+++ voice_assistant_project/voice_assistant/openai_integration.py
@@ -1,12 +1,16 @@
 import openai
 import datetime
 from textblob import TextBlob
-
+import datetime
+import dateparser
+import spacy
+from .calender_integration import get_calendar_service, get_date_info, create_reminder
 from .database import insert_message, retrieve_database_history, retrieve_memory_history
 
 openai.api_key = "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"
 openai.Model.retrieve("gpt-3.5-turbo")
 
+nlp = spacy.load("en_core_web_sm")
 
 def handle_question(question, conversation_history, memory_history, conn):
     current_time = datetime.datetime.now()
@@ -18,6 +22,17 @@ def handle_question(question, conversation_history, memory_history, conn):
         else retrieve_database_history(conn, 5)
     )
 
+    #check for date related question
+    doc = nlp(question)
+    has_date_entity = any(token.ent_type_ == "DATE" for token in doc)
+
+    #if question has date entity, use get_date_info
+    if has_date_entity:
+        print("date related question")
+        date_info = get_date_info(question)
+        if date_info != "Sorry, I dont understand the question.":
+            return date_info
+    
     recall_phrases = ["remember when", "recall", "search for"]
     first_words = " ".join(question.lower().split()[:3])
 
@@ -33,6 +48,7 @@ def handle_question(question, conversation_history, memory_history, conn):
 
     history_str = "\n".join(f"{entry[1]}: {entry[2]}" for entry in filtered_history)
     sentiment = analyze_sentiment(question)
+    
     answer = generate_response(question, history_str, sentiment)
 
     if not answer.strip():
