2826276e74541b01dd1858ac9c32b76b66a35112
Zeebs <simplyninjaz@gmail.com>
2023-05-22 20:07:13 -0400

    double click to copy chat

--- voice_assistant_project/gui.py
+++ voice_assistant_project/gui.py
@@ -7,23 +7,40 @@ import threading
 from voice_assistant.speech import synthesize_speech, play_speech_threaded
 from voice_assistant.openai_integration import handle_question
 from voice_assistant import assistant
-from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivy.core.window import Window
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.list import OneLineListItem
-from kivy.factory import Factory
-from kivy.clock import Clock
-from kivy.uix.label import Label
-from kivy.metrics import dp
+from kivy.core.clipboard import Clipboard
+from kivy.uix.button import Button
 from kivy.config import Config
+from pynput.keyboard import Controller
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+from kivy.factory import Factory
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivy.lang import Builder
+
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
+class WrappedButton(Button):
+    def __init__(self, **kwargs):
+        # Call the parent constructor with the same arguments
+        super().__init__(**kwargs)
+        # Make the button look like a label by removing its background
+        self.background_normal = ''
+        self.background_color = (0, 0, 0, 0)
+        # Align the text to the left (you might need to adjust this depending on your design)
+        self.halign = 'left'
+
+    def on_release(self):
+        # When the button is clicked, copy its text to the clipboard
+        Clipboard.copy(self.text)
 
 class VoiceAssistantUI(BoxLayout):
     def __init__(self, app, **kwargs):
         super().__init__(**kwargs)
         self.app = app
+
+        self.keyboard = Controller()
 
     def process_query(self, query):
         self.ids.user_input.text = ""
@@ -50,6 +67,10 @@ class VoiceAssistantUI(BoxLayout):
 
     def set_focus(self, dt):
         self.ids.user_input.focus = True
+
+    def simulate_key_press(self, key):
+        self.keyboard.press(key)
+        self.keyboard.release(key)
   
     def clear_chat_box(self):
         def clear(dt):
@@ -71,12 +92,14 @@ class VoiceAssistantUI(BoxLayout):
             if 'welcome_message' in self.ids and self.ids.welcome_message.parent is not None:
                 self.ids.output_container.remove_widget(self.ids.welcome_message)
 
-            assistant_message_item = Factory.WrappedLabel(text=f"Assistant: {assistant_message}")
-            
+            # Change this to create an instance of WrappedButton instead of WrappedLabel
+            assistant_message_item = WrappedButton(text=f"Assistant: {assistant_message}")
+
             if user_message:
-                user_message_item = Factory.WrappedLabel(text=f"User: {user_message}")
+                # Same change here
+                user_message_item = WrappedButton(text=f"User: {user_message}")
                 self.ids.output_container.add_widget(user_message_item)
-            
+
             self.ids.output_container.add_widget(assistant_message_item)
             Clock.schedule_once(lambda dt: self.ids.scroll_view.scroll_to(assistant_message_item), 0.1)
             self.ids.scroll_view.scroll_to(assistant_message_item)
